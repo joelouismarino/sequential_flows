0b2f9d54239330cc4e9179122e260eef70a116e9
Lei Chen <arzela@LeideAir.hitronhub.home>
2019-09-24 20:51:52 -0700

    add kth 2 flow setting

---
+++ kth_2flow_config/data_config.py
@@ -0,0 +1,8 @@
+data_config = {'dataset_name': 'kth_actions',
+               'data_path': '/local-scratch/chenleic/Data',
+               'add_noise': True,
+               # 'num_digits': 2,
+               'sequence_length': 13,
+               'eval_length': 10,
+               'img_hz_flip': False,
+               'img_size': 64}
